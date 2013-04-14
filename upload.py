d0dc9d354ba97b6528c909f1772a1f8d73790f63
Jon Hoffman <jonhoffman@gmail.com>
2013-04-14 00:36:51 -0400

    file cleanup

--- upload.py
+++ upload.py
@@ -11,11 +11,11 @@ from pprint import pprint
 KEY = os.environ['aws_key']
 SECRET = os.environ['aws_secret']
 
-def do_upload(key, fp):
+def do_upload(key, file_pointer):
     cache_time = 60 * 5
     now = datetime.now()
     expire_dt = now + timedelta(seconds=cache_time * 1.5)
-    key.set_contents_from_file(fp, policy="public-read",
+    key.set_contents_from_file(file_pointer, policy="public-read",
       headers={
         'Cache-Control': 'max-age=%d, must-revalidate' % int(cache_time),
         'Expires': expire_dt.strftime("%a, %d %b %Y %H:%M:%S GMT")
@@ -31,24 +31,21 @@ def upload_dir_to_s3(bucket, src_dir):
       relative_path = os.path.relpath(os.path.join(path,file), src_dir)
       key = b.get_key(relative_path) or b.new_key(relative_path)
 
-      file_name = os.path.join(path, file)
-      fp = open(file_name)
-
-      if not key.etag:
-        print("Creating: " + key.key)
-        do_upload(key, fp)
-      else:
-        md5 = key.compute_md5(fp)
-        etag = key.etag or ''
-        # for some weird reason, etags are quoted, strip them
-        etag = etag.strip('"').strip("'")
-        if etag not in md5:
-            do_upload(key, fp)
-            print("Updating: " + key.key)
+      with open(os.path.join(path, file)) as file_pointer:
+        if not key.etag:
+          print("Creating: " + key.key)
+          do_upload(key, file_pointer)
         else:
-            print("Skipping: " + key.key)
+          md5 = key.compute_md5(file_pointer)
+          etag = key.etag or ''
+          # for some weird reason, etags are quoted, strip them
+          etag = etag.strip('"').strip("'")
+          if etag not in md5:
+              do_upload(key, file_pointer)
+              print("Updating: " + key.key)
+          else:
+              print("Skipping: " + key.key)
 
-      
 
 if __name__ == "__main__":
     upload_dir_to_s3('sabaletters', 'site')
